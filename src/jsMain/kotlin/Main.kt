fae742c21d789f4167af8db6168e9443b7709346
Nathan Schulzke <nschulzke@gmail.com>
2021-08-13 09:30:20 -0600

    Remove the concrete classes, as they aren't necessary to demonstrate the problem

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -7,9 +7,7 @@ import kotlinx.serialization.json.Json
 interface Interface
 
 @Serializable
-sealed class Subtype : Interface {
-    data class SubSubType(val value: String)
-}
+sealed class Subtype : Interface
 
 fun main() {
     val module = SerializersModule {
