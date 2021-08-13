876ccb6c86289b105546ab58eb097a18180d5465
Nathan Schulzke <nschulzke@gmail.com>
2021-08-13 09:27:33 -0600

    Remove the code for actually serializing, as it's not necessary to reproduce the problem

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,6 +1,4 @@
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
 import kotlinx.serialization.modules.subclass
@@ -22,10 +20,4 @@ fun main() {
     val json = Json {
         serializersModule = module
     }
-
-    val value1 = Subtype.SubSubType("test")
-    val string = json.encodeToString(value1)
-    val value2 = json.decodeFromString<Interface>(string)
-
-    println(value2.toString())
 }
